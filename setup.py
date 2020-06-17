bbf2069095480135ed3d5b07ba4c34d80193997d
Mark Grivainis <markgrivainis@gmail.com>
2020-06-17 15:49:32 -0400

    Updated version number to 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SAMPEI",
-    version="0.0.2",
+    version="0.0.3",
     author="Zhi Li",
     author_email="Zhi.Li@nyulangone.org",
     description="SAMPEI",
