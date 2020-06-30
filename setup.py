5997bad2cd6858ff1075ba59de697d2897749566
lz870718 <lizhi718@gmail.com>
2020-06-30 13:48:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SAMPEI",
-    version="0.0.6",
+    version="0.0.7",
     author="Zhi Li",
     author_email="Zhi.Li@nyulangone.org",
     description="SAMPEI",
